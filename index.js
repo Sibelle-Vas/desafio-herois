41361a8aeddfc28d96035dad82f419ee9ecf9c53
Sibelle-Vas <sibelle.vasconcelos401@gmail.com>
2023-09-27 10:54:00 -0300

    Meu segundo commit

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("oi")
+let nomeHeroi = "Chris"
+let quantidadeExperiencia = 10001
+
+if (quantidadeExperiencia < 1000) {
+    console.log ("O herói de nome " + nomeHeroi + " está no nível de ferro!")
+}
+else if (quantidadeExperiencia >= 1001 && quantidadeExperiencia <= 2000){
+  console.log ("O herói de nome " + nomeHeroi + " está no nível de bronze!")
+}
+else if (quantidadeExperiencia >= 2001 && quantidadeExperiencia <= 5000){
+  console.log ("O herói de nome " + nomeHeroi + " está no nível de prata!")
+}
+else if (quantidadeExperiencia >= 5001 && quantidadeExperiencia <= 7000){
+  console.log ("O herói de nome " + nomeHeroi + " está no nível de ouro!")
+}
+else if (quantidadeExperiencia >= 7001 && quantidadeExperiencia <= 8000){
+  console.log ("O herói de nome " + nomeHeroi + " está no nível de platina!")
+}
+else if (quantidadeExperiencia >= 8001 && quantidadeExperiencia <= 9000){
+  console.log ("O herói de nome " + nomeHeroi + " está no nível de ascendente!")
+}
+else if (quantidadeExperiencia >= 9001 && quantidadeExperiencia <= 10000){
+  console.log ("O herói de nome " + nomeHeroi + " está no nível de imortal!")
+}
+else if (quantidadeExperiencia >= 10001){
+  console.log ("O herói de nome " + nomeHeroi + " está no nível de radiante!")
+}
